c59a8963b733805c62c061158133afa66f579e53
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-18 15:42:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,6 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each.with_index(1) {|name, index| room_assignments.push("Hello, #{name}! You'll be assigned to room #{index}!")}
+  room_assignments
 end
 
