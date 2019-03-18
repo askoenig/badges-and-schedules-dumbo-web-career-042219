ee221d566e8747c6e98101a5e7ae6ac7e4ace6d2
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-18 15:01:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,4 +4,6 @@ end
 
 def batch_badge_creator(array_names)
   for array_names.size do
-  array_names.each {|x| "Hello, my name is #{name}."}
+  array_names.each {|x| puts "Hello, my name is #{name}."}
+end 
+
